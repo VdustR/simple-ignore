a899ffc62114efeb13d7206b485b93269f2b3684
VdustR <vdustr@gmail.com>
2024-02-11 19:42:43 +0800

    build: fix

--- packages/simple-ignore/build.config.ts
+++ packages/simple-ignore/build.config.ts
@@ -1,5 +1,33 @@
 import { defineBuildConfig } from "unbuild";
 
-export default defineBuildConfig({
-  failOnWarn: false,
-});
+import packageJson from "./package.json";
+
+const externals = Object.keys(packageJson.dependencies);
+
+export default defineBuildConfig([
+  {
+    entries: [
+      {
+        builder: "rollup",
+        input: "./src/index.ts",
+      },
+    ],
+    rollup: {
+      emitCJS: true,
+    },
+    externals,
+    declaration: true,
+  },
+  {
+    entries: [
+      {
+        builder: "rollup",
+        input: "./src/cli.ts",
+      },
+    ],
+    rollup: {
+      emitCJS: true,
+    },
+    externals: ["../package.json"],
+  },
+]);
